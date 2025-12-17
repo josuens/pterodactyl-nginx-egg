8fc27928ed106690fbd0481c132e6c80b4fbb3bf
Josue Navarro <65885730+josuens@users.noreply.github.com>
2025-12-16 23:11:54 -0600

    Update start.sh

--- modules/nginx/start.sh
+++ modules/nginx/start.sh
@@ -52,10 +52,10 @@ sleep 1
 # Copyright footer
 echo -e " "
 echo -e "\033[0;34m───────────────────────────────────────────────\033[0m"
-echo -e "\033[1;36m© 2023-2025 by Ym0T - Thanks to all contributors\033[0m"
-echo -e "\033[1;36mScript: \033[4;34mhttps://github.com/Ym0T\033[0m"
-echo -e "\033[1;36mLicensed under the MIT License.\033[0m"
-echo -e "\033[1;36mSee the LICENSE file for details.\033[0m"
+echo -e "\033[1;36mINFO IMPORTANT BY HOSTBU\033[0m"
+echo -e "\033[1;36mTO GET A FREE SUBDOMAIN\033[0m"
+echo -e "\033[1;36mCREATE A TICKET IN HOSTBU SUPPORT\033[0m"
+echo -e "\033[1;36mSupport: \033[4;34mhttps://hostbu.com/support\033[0m"
 echo -e "\033[0;34m───────────────────────────────────────────────\033[0m"
 
 # Start NGINX
